806d6bed0abce7c880f1de9d149841fea11a0fd7
DARKABYSS\Lucan <lewis9736_pearson@hotmail.co.uk>
2017-11-30 15:27:46 +0000

    Delegating To Components 30/11/17

---
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -0,0 +1,11 @@
+// Lone Wolf Studio(L J K Pearson)
+
+#include "BattleTank.h"
+#include "TankPlayerController.h"
+
+
+
+ATank* ATankPlayerController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
---
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -0,0 +1,21 @@
+// Lone Wolf Studio(L J K Pearson)
+
+#pragma once
+
+#include "Tank.h"
+#include "GameFramework/PlayerController.h"
+#include "TankPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API ATankPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+public:
+	ATank* GetControlledTank() const;
+	
+	
+};
