775f73f56d194b32a925c560ad2e8448998a441a
DARKABYSS\Lucan <lewis9736_pearson@hotmail.co.uk>
2017-12-06 10:42:17 +0000

    Creating An AI Controller Class 06/12/17

---
+++ BattleTank/Source/BattleTank/Public/TankAIController.h
@@ -0,0 +1,21 @@
+// Lone Wolf Studio(L J K Pearson)
+
+#pragma once
+
+#include "Tank.h"
+#include "AIController.h"
+#include "TankAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API ATankAIController : public AAIController
+{
+	GENERATED_BODY()
+
+	virtual void BeginPlay() override;
+	
+	ATank* GetControlledTank() const;
+	
+};
