1124306160a2fce687f59dbec0876d5e9c7f6625
DARKABYSS\Lucan <lewis9736_pearson@hotmail.co.uk>
2017-12-08 13:57:20 +0000

    Unify Player & AI Aiming 08/12/17

--- BattleTank/Source/BattleTank/Public/TankAIController.h
+++ BattleTank/Source/BattleTank/Public/TankAIController.h
@@ -14,8 +14,11 @@ class BATTLETANK_API ATankAIController : public AAIController
 {
 	GENERATED_BODY()
 
+private:
 	virtual void BeginPlay() override;
 	
+	virtual void Tick(float DeltaSeconds) override;
+
 	ATank* GetControlledTank() const;
 	
 	ATank* GetPlayerTank() const;
