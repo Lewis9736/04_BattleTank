bda9ebf60a1d0bb1808e9f10cde89cd49c0c188c
DARKABYSS\Lucan <lewis9736_pearson@hotmail.co.uk>
2017-12-09 10:38:48 +0000

    Blueprint Callable() 09/12/17

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -38,5 +38,9 @@ void ATank::SetupPlayerInputComponent(class UInputComponent* InputComponent)
 void ATank::AimAt(FVector HitLocation)
 {
 	TankAimingComponent->AimAt(HitLocation);
-	
+}
+
+void ATank::SetBarrelReference(UStaticMeshComponent* BarrelToSet)
+{
+	TankAimingComponent->SetBarrelReference(BarrelToSet);
 }
