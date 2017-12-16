6e22a342ae311ccdaddde770d2d403e0d348bb66
DARKABYSS\Lucan <lewis9736_pearson@hotmail.co.uk>
2017-12-16 07:30:14 +0000

    CHALLENGE Turret Rotation Pt 2

--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -12,7 +12,7 @@ UTankAimingComponent::UTankAimingComponent()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	bWantsBeginPlay = true;
-	PrimaryComponentTick.bCanEverTick = true; // TODO Should this really tick?
+	PrimaryComponentTick.bCanEverTick = false; 
 
 											  // ...
 }
@@ -45,8 +45,8 @@ void UTankAimingComponent::AimAt(FVector HitLocation, float LaunchSpeed)
 		false,
 		0,
 		0,
-		ESuggestProjVelocityTraceOption::DoNotTrace // Paramater must be present to prevent bug
-													// TODO report bug
+		ESuggestProjVelocityTraceOption::DoNotTrace 
+													
 	);
 
 	if (bHaveAimSolution)
@@ -72,4 +72,5 @@ void UTankAimingComponent::MoveBarrelTowards(FVector AimDirection)
 	auto DeltaRotator = AimAsRotator - BarrelRotator;
 
 	Barrel->Elevate(DeltaRotator.Pitch);
+	Turret->Rotate(DeltaRotator.Yaw);
 }
