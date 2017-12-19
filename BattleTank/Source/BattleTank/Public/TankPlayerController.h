fccc74593b674d34148c602f3876d3bab167626e
DARKABYSS\Lucan <lewis9736_pearson@hotmail.co.uk>
2017-12-19 11:52:30 +0000

    EditAnywhere vs EditDefaultsOnly

--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -2,39 +2,41 @@
 
 #pragma once
 
-
 #include "GameFramework/PlayerController.h"
-#include "TankPlayerController.generated.h"
+#include "TankPlayerController.generated.h" // Must be the last include
 
 class ATank;
 
 /**
- * 
- */
+*
+*/
 UCLASS()
 class BATTLETANK_API ATankPlayerController : public APlayerController
 {
 	GENERATED_BODY()
-	
+
 private:
 	ATank* GetControlledTank() const;
-	
+
 	virtual void BeginPlay() override;
 
 	virtual void Tick(float DeltaTime) override;
 
+	// Start the tank moving the barrel so that a shot would hit where
+	// the crosshair intersects the world
 	void AimTowardsCrosshair();
 
+	// Return an OUT parameter, true if hit landscape
 	bool GetSightRayHitLocation(FVector& HitLocation) const;
-		
-	UPROPERTY(EditAnywhere)
-	float CrosshairXLocation = 0.5;
 
-	UPROPERTY(EditAnywhere)
-	float CrosshairYLocation = 0.3333;
+	UPROPERTY(EditDefaultsOnly)
+		float CrosshairXLocation = 0.5;
 
-	UPROPERTY(EditAnywhere)
-	float LineTraceRange = 1000000;
+	UPROPERTY(EditDefaultsOnly)
+		float CrosshairYLocation = 0.3333;
+
+	UPROPERTY(EditDefaultsOnly)
+		float LineTraceRange = 1000000;
 
 	bool GetLookDirection(FVector2D ScreenLocation, FVector& LookDirection) const;
 	bool GetLookVectorHitLocation(FVector LookDirection, FVector& HitLocation) const;
